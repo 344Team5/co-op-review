a96d515727d505cbe4806ae713e01bd0afb24057
David Grzebinski <dwg7486@rit.edu>
2018-03-06 00:08:10 -0500

    Fake login and more project structure

--- README.md
+++ README.md
@@ -1,14 +1,19 @@
 # co-op-review
-####RIT SWEN 344 Team 5 Web Project
+###RIT SWEN 344 Team 5 Web Project
+
 A replacement for RIT's current co-op review system.
 
 **Team Members:**
 * David Grzebinski (dwg7486@rit.edu)
-* Christian Greaves (cxg4749@rit.edu)
-* Quintin Reed (qar4778@rit.edu)
+  `Project Coordinator` `VM Manager`
+* Christian Greaves (cxg4749@rit.edu) 
+  `Testing Coordinator`
+* Quintin Reed (qar4778@rit.edu) 
+  `Design Coordinator`
 * Stefan Marchhart (stm4083@g.rit.edu)
+  `Development Coordinator`
 * Tim Coon (tec3246@g.rit.edu)
-  
+  `Requirements Coordinator`
   
 
   
--- src/main/java/CoopReview.java
+++ src/main/java/CoopReview.java
@@ -15,15 +15,45 @@ import java.util.Map;
  */
 public class CoopReview implements SparkApplication {
     public void init() {
+        MustacheTemplateEngine templateEngine = new MustacheTemplateEngine();
+
         staticFiles.location("/public"); // set static files location to /public in resources
 
-        get("/", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            model.put("name", "Student");
-            return new MustacheTemplateEngine().render(
+        Map<String, Object> model = new HashMap<>();
+
+        get("/", (request, response) -> {
+            return templateEngine.render(
                     new ModelAndView(model, "homepage.mustache")
             );
         });
+
+        post("/", ((request, response) -> { // this will be login in R2
+            String username = request.queryParams("username");
+            model.put("user", username); // fake login
+            response.redirect("/coops");
+            return "";
+        }));
+
+        get("/coops", ((request, response) -> { // "logged in" page
+            return templateEngine.render(
+                    new ModelAndView(model, "coops.mustache")
+            );
+        }));
+
+        /* Example path route for API
+        path("/api", () -> {
+            path("/email", () -> {
+                post("/add",       EmailApi.addEmail);
+                put("/change",     EmailApi.changeEmail);
+                delete("/remove",  EmailApi.deleteEmail);
+            });
+            path("/username", () -> {
+                post("/add",       UserApi.addUsername);
+                put("/change",     UserApi.changeUsername);
+                delete("/remove",  UserApi.deleteUsername);
+            });
+        });
+        */
     }
 
     public static void main(String[] args) {
