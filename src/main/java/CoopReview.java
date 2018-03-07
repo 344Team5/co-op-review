205c3b8aa43fba6af97c5a5f8352677e325f55bf
dwg7486 <dwg7486@rit.edu>
2018-03-07 11:01:09 -0500

    We don't need that, forget I did that

--- src/main/java/CoopReview.java
+++ src/main/java/CoopReview.java
@@ -1,6 +1,5 @@
 import static spark.Spark.*;
 
-import api.Coops;
 import spark.ModelAndView;
 import spark.servlet.SparkApplication;
 import spark.template.mustache.MustacheTemplateEngine;
@@ -51,57 +50,6 @@ public class CoopReview implements SparkApplication {
 
         get("/review/:token", ((request, response) -> null)); // external reviewer
         post("/review/:token", (request, response) -> "");
-        /* End Front End Routes */
-
-        /* API Routes */
-        path("api/v1", () -> {
-            path("/students", () -> {
-                get("/", (request, response) -> null); // get all students
-                post("/", (request, response) -> null); // create a student
-
-                path("/:sid", () -> { // one student
-                    get("/", (request, response) -> null); // read
-                    put("/", (request, response) -> null); // update/replace
-                    patch("/", (request, response) -> null); // update/modify
-                    delete("/", (request, response) -> null); // delete
-                });
-
-            });
-            path("/employers", () -> {
-                get("/", (request, response) -> null); // get all employers
-                post("/", (request, response) -> null); // create an employer
-
-                path("/:eid", () -> { // one employer
-                    get("/", (request, response) -> null); // read
-                    put("/", (request, response) -> null); // update/replace
-                    patch("/", (request, response) -> null); // update/modify
-                    delete("/", (request, response) -> null); // delete
-
-                    get("/name", (request, response) -> null); // get employer name
-                    get("/address", (request, response) -> null); // get employer address
-                    get("/website", ((request, response) -> null)); // get employer website
-                });
-            });
-
-            path("/coops", () -> {
-                get("/", ((request, response) -> null)); // get all co-ops
-                post("/", ((request, response) -> null)); // create a co-op
-
-                path("/:cid", () -> { // one co-op
-                    get("/", (request, response) -> null); // read
-                    put("/", (request, response) -> null); // update/replace
-                    patch("/", (request, response) -> null); // update/modify
-                    delete("/", (request, response) -> null); // delete
-
-                    get("/student", (request, response) -> null); // get the co-op's associated student
-                    get("/employer", ((request, response) -> null)); // get the co-op's associated employer
-                    get("/studentEvaluation", ((request, response) -> null)); // get the student evaluation
-                    get("/coopWorkReport", ((request, response) -> null)); // get the co-op work report
-                    get("/employerReview", (request, response) -> null); //get the employer review
-                });
-            });
-        });
-        /* End API Routes */
     }
 
     public static void main(String[] args) {
