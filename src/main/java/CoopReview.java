6fafb3a928daa9c5a4214866021d29f459977b8d
dwg7486 <dwg7486@rit.edu>
2018-04-23 10:59:51 -0400

    Add OAuth stuff

--- src/main/java/CoopReview.java
+++ src/main/java/CoopReview.java
@@ -6,7 +6,17 @@ import db.FakeDB;
 import model.Coop;
 import model.Student;
 import org.json.JSONObject;
+import org.kohsuke.github.GitHub;
+import org.pac4j.core.config.Config;
+import org.pac4j.core.profile.CommonProfile;
+import org.pac4j.core.profile.ProfileManager;
+import org.pac4j.oauth.profile.github.GitHubProfile;
+import org.pac4j.sparkjava.SecurityFilter;
+import org.pac4j.sparkjava.SparkWebContext;
+import org.pac4j.sparkjava.LogoutRoute;
 import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
 import spark.servlet.SparkApplication;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
@@ -41,6 +51,57 @@ public class CoopReview implements SparkApplication {
         frontEndPageRoutes(templateEngine);
         errorPageRoutes(templateEngine);
         internalAPIRoutes(db);
+        gitHubOAuthSetup(templateEngine);
+
+
+    }
+
+    private void gitHubOAuthSetup(HandlebarsTemplateEngine templateEngine) {
+        HashMap<String,String> envVars =
+                getNeededEnvVars(new String []{ "GITHUB_CLIENT_ID",
+                        "GITHUB_CLIENT_SECRET",
+                        "GITHUB_CALLBACK_URL",
+                        "APPLICATION_SALT"});
+
+        Config config = new
+                OAuthConfigFactory(envVars.get("GITHUB_CLIENT_ID"),
+                envVars.get("GITHUB_CLIENT_SECRET"),
+                envVars.get("GITHUB_CALLBACK_URL"),
+                envVars.get("APPLICATION_SALT"),
+                templateEngine).build();
+
+        final SecurityFilter
+                githubFilter = new SecurityFilter(config, "GithubClient", "", "");
+
+        get("/",
+                (request, response) -> new ModelAndView(buildModel(request,response),"login.hbs"),
+                templateEngine);
+
+        before("/login", githubFilter);
+
+        get("/login",
+                (request, response) -> new ModelAndView(buildModel(request,response),"login.hbs"),
+                templateEngine);
+
+        get("/logout", new LogoutRoute(config, "/"));
+
+        get("/session",
+                (request, response) -> new ModelAndView(buildModel(request,response),
+                        "session.mustache"),
+                templateEngine);
+
+        get("/github",
+                (request, response) ->
+                        new ModelAndView(addGithub(buildModel(request,response),request,response),
+                                "github.mustache"),
+                templateEngine);
+
+        final org.pac4j.sparkjava.CallbackRoute callback =
+                new org.pac4j.sparkjava.CallbackRoute(config);
+
+        get("/callback", callback);
+        post("/callback", callback);
+
     }
 
     private void frontEndPageRoutes(HandlebarsTemplateEngine templateEngine) {
@@ -54,6 +115,7 @@ public class CoopReview implements SparkApplication {
                 res.redirect(path.substring(0, path.length() - 1));
         });
         */
+
         before((req, res) -> { // redirect requests with trailing '/'
             System.out.println("Received request: " + req.url());
         });
@@ -300,7 +362,7 @@ public class CoopReview implements SparkApplication {
      *
      * @return returns connection to the database
      */
-    private static Connection getConnectionAlt() throws URISyntaxException, SQLException {
+    private Connection getConnectionAlt() throws URISyntaxException, SQLException {
         URI dbUri = new URI(System.getenv("JDBC_DATABASE_URL"));
 
         String username = dbUri.getUserInfo().split(":")[0];
@@ -308,5 +370,93 @@ public class CoopReview implements SparkApplication {
         String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ":" + dbUri.getPort() + dbUri.getPath();
 
         return DriverManager.getConnection(dbUrl, username, password);
+    }
+
+    private static java.util.List<CommonProfile> getProfiles(final Request request,
+                                                             final Response response) {
+        final SparkWebContext context = new SparkWebContext(request, response);
+        final ProfileManager manager = new ProfileManager(context);
+        return manager.getAll(true);
+    }
+
+    /**
+     add github information to the session
+     */
+    private static Map addGithub(Map model, Request request, Response response) {
+        GitHubProfile ghp = ((GitHubProfile)(model.get("ghp")));
+        if (ghp == null) {
+            System.out.println("No github profile");
+            return model;
+        }
+        try {
+            String accessToken = ghp.getAccessToken();
+            GitHub gh = null;
+            gh =  GitHub.connect( model.get("userid").toString(), accessToken);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return model;
+    }
+
+    private static Map buildModel(Request request, Response response) {
+
+        final Map model = new HashMap<String,Object>();
+
+        Map<String, Object> map = new HashMap<String, Object>();
+        for (String k: request.session().attributes()) {
+            Object v = request.session().attribute(k);
+            map.put(k,v);
+        }
+
+        model.put("session", map.entrySet());
+
+        java.util.List<CommonProfile> userProfiles = getProfiles(request,response);
+
+        map.put("profiles", userProfiles);
+
+        try {
+            if (userProfiles.size()>0) {
+                CommonProfile firstProfile = userProfiles.get(0);
+                map.put("firstProfile", firstProfile);
+
+                GitHubProfile ghp = (GitHubProfile) firstProfile;
+                model.put("ghp", ghp);
+                model.put("userid",ghp.getUsername());
+                model.put("name",ghp.getDisplayName());
+                model.put("avatar_url",ghp.getPictureUrl());
+                model.put("email",ghp.getEmail());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return model;
+    }
+
+    /**
+     return a HashMap with values of all the environment variables
+     listed; print error message for each missing one, and exit if any
+     of them is not defined.
+     */
+
+    public static HashMap<String,String> getNeededEnvVars(String [] neededEnvVars) {
+        HashMap<String,String> envVars = new HashMap<String,String>();
+
+
+        for (String k:neededEnvVars) {
+            String v = System.getenv(k);
+            envVars.put(k,v);
+        }
+
+        boolean error=false;
+        for (String k:neededEnvVars) {
+            if (envVars.get(k)==null) {
+                error = true;
+                System.err.println("Error: Must define env variable " + k);
+            }
+        }
+        if (error) { System.exit(1); }
+
+        return envVars;
     }
 }
