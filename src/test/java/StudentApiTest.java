8de23217be7eaaf1ed45a14d1122d34c32e0e018
tec3246 <tec3246@rit.edu>
2018-04-29 12:57:41 -0400

    Add initial postman exported collection file

---
+++ src/test/java/StudentApiTest.java
@@ -0,0 +1,19 @@
+import api.StudentApi;
+import org.junit.jupiter.api.Test;
+import spark.Request;
+import spark.Response;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class StudentApiTest {
+
+    @Test
+    void getAll() {
+        assertTrue(true);
+    }
+
+    @Test
+    void get() {
+        assertTrue(true);
+    }
+}
