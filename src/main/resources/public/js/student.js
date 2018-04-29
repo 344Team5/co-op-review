d548a0293ec42609b68981de1b1b30eeda81a7be
David Grzebinski <dwg7486@rit.edu>
2018-04-29 12:13:31 -0400

    Major appearance improvements

--- src/main/resources/public/js/student.js
+++ src/main/resources/public/js/student.js
@@ -1,20 +1,25 @@
-$.get({
-    url: "/api/v1/students/" + sid + "/coops",
-    success: function(data) {
-        getById("coops").innerHTML = "";
-        console.log(data);
-        $.each(data.coops, function(i, coop) {
-            //console.log(item);
-            let li = document.createElement("li");
-            let a = document.createElement("a");
-            a.style.display = "none";
-            a.href = "/coops/" + coop.id;
-            setEmployerName(a, coop.employer_id);
-            li.append(a);
-            getById("coops").append(li);
-        });
-    }
-});
+updateStudentInfo();
+
+function updateStudentInfo() {
+    $.get({
+        url: "/api/v1/students/" + sid + "/coops",
+        success: function (data) {
+            getById("coops").innerHTML = "";
+            //console.log(data);
+            getById("greeting").innerHTML = "Welcome, " + data.student[0].name;
+            $.each(data.coops, function (i, coop) {
+                //console.log(item);
+                let li = document.createElement("li");
+                let a = document.createElement("a");
+                a.style.display = "none";
+                a.href = "/coops/" + coop.id;
+                setEmployerName(a, coop.employer_id);
+                li.append(a);
+                getById("coops").append(li);
+            });
+        }
+    });
+}
 
 function setEmployerName(a, id) {
     var name = "Error retrieving company name."
@@ -33,4 +38,8 @@ function setEmployerName(a, id) {
 function setInnerHTML(element, html) {
     element.innerHTML = html;
     element.style.display = "";
+}
+
+function setStatusMessage(msg) {
+    getById("status").innerHTML = msg;
 }
