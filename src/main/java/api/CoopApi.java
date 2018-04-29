b92e716552b5ecb723c9234ded002ad066c9b023
tec3246 <tec3246@rit.edu>
2018-04-28 22:56:30 -0400

    added some coop api functions

--- src/main/java/api/CoopApi.java
+++ src/main/java/api/CoopApi.java
@@ -3,9 +3,33 @@ package api;
 import spark.Request;
 import spark.Response;
 
+import java.sql.*;
+
 public class CoopApi extends DatabaseApi {
     public static Object getCoops(Request request, Response response) {
-        return "Get all Students";
+        Object result = "";
+        Connection c = db();
+        Statement st = null;
+        if (c != null) {
+            try {
+                st = db().createStatement();
+                ResultSet rs = st.executeQuery("SELECT * FROM coops;");
+                result = getSQLQueryResultsJson(rs, "id", "start_date", "end_date", "work_report",
+                        "student_eval", "eval_token", "student_uid", "employer_id");
+                response.type("application/json");
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (st != null) {
+                    try {
+                        st.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        return result;
     }
 
     public static Object addCoop(Request request, Response response) {
@@ -13,7 +37,30 @@ public class CoopApi extends DatabaseApi {
     }
 
     public static Object getCoop(Request request, Response response) {
-        return "Get a Student";
+        Object result = "";
+        Connection c = db();
+        PreparedStatement st = null;
+        if (c != null) {
+            try {
+                st = db().prepareStatement("SELECT * FROM coops where id = CAST(? AS INTEGER);");
+                st.setString(1, request.params().get(":cid"));
+                ResultSet rs = st.executeQuery();
+                result = getSQLQueryResultsJson(rs, "id", "start_date", "end_date", "work_report",
+                        "student_eval", "eval_token", "student_uid", "employer_id");
+                response.type("application/json");
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (st != null) {
+                    try {
+                        st.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        return result;
     }
 
     public static Object putCoop(Request request, Response response) {
