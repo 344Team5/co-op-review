501f9c695a405110ac450bdcd3a7445fc4aad481
David Grzebinski <dwg7486@rit.edu>
2018-04-29 22:27:33 -0400

    Push current work

--- src/main/resources/public/js/student.js
+++ src/main/resources/public/js/student.js
@@ -7,10 +7,42 @@ function updateStudentInfo() {
             getById("coops").innerHTML = "";
             console.log(data);
             try {
-                let studentUid = data.student[0].uid;
-
-
+                $.each(data.coops, function(i, coop) {
+                    //console.log(coop);
+                    let li = document.createElement("li");
+                    let a = document.createElement("a");
+                    let span = document.createElement("span");
+                    span.id
+                    setEmployerName(getById("employerName"), coop.employer_id);
+                    a.href = "/coops/" + coop.id;
+                    a.innerHTML = "Co-op at " ;
+                    a.append(span);
+                    li.append(a);
+                    getById("coops").append(li);
+                });
             } catch (err) {}
+        }
+    });
+}
+
+function submitForm() {
+    let data = {};
+    data.start_date = getById("startDate").value;
+    data.end_date = getById("startDate").value;
+    data.employer_id = 1;
+    data.student_uid = sid;
+    $.post({
+        url: "/api/v1/coops",
+        data: JSON.stringify(data),
+        success: function(data) {
+            //console.log(data);
+            if (data === "success") {
+                getById("status").innerHTML = "Co-op submitted successfully."
+                getById("status").style.color = "green";
+            } else {
+                getById("status").innerHTML = "Something went wrong."
+                getById("status").style.color = "red";
+            }
         }
     });
 }
